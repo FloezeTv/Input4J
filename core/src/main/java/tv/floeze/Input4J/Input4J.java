92788f98ecf6c8349418ed7ae108db0805ebc023
FloezeTv <mathematikschlumpf@gmail.com>
2021-01-16 19:19:46 +0100

    Added main class

---
+++ core/src/main/java/tv/floeze/Input4J/Input4J.java
@@ -0,0 +1,19 @@
+package tv.floeze.Input4J;
+
+/**
+ * Input4J is the main class to handle input from various sources.
+ * 
+ * @author Floeze
+ *
+ * @param <T> Class of identifier
+ */
+public class Input4J<T> {
+
+	/**
+	 * Creates a new Instance of {@link Input4J}
+	 */
+	public Input4J() {
+
+	}
+
+}
